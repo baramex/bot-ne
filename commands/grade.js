a4f0088771b2cec78b37103bb7000fff5cde051f
baramex <contact@baramex.me>
2022-02-20 11:28:24 +0100

    partial added lang & agree & grade + start added names verif and remove config.json

--- commands/grade.js
+++ commands/grade.js
@@ -1,15 +1,15 @@
-module.exports.run = (bot, interaction, lang, db) => {
+module.exports.run = async (bot, interaction, lang, db) => {
     var member = interaction.options.getMember("mention", false);
 
     var m = member || interaction.member;
 
-    var grades = bot.getGradesMember(m.id);
+    var grades = await bot.getGradesMember(m.id).catch(console.error);
 
     var embed = new bot.libs.discord.MessageEmbed()
         .setColor(bot.infoColor)
         .setTitle(":dagger: | New Empires - grade")
         .setFooter({ text: bot.footerAuthor.text + " | " + lang.toUpperCase(), iconURL: bot.footerAuthor.iconURL })
-        .addField("Grade", (lang == "fr" ? ("Le.s grade.s de <@" + m.id + "> est.sont ") : ("<@" + m.id + ">'s grade.s is.are ")) + "**" + grades + "**")
+        .addField("Grade", (lang == "fr" ? ("Le.s grade.s de <@" + m.id + "> est.sont ") : ("<@" + m.id + ">'s grade.s is.are ")) + "**" + grades.join(" & ") + "**")
         .setThumbnail(m.user.avatarURL());
 
     interaction.reply({ embeds: [embed] });
--- commands/lang.js
+++ commands/lang.js
@@ -1,15 +1,15 @@
-module.exports.run = (bot, interaction, lang, db) => {
+module.exports.run = async (bot, interaction, lang, db) => {
     var member = interaction.options.getMember("mention", false);
 
     var m = member || interaction.member;
 
-    var lStr = bot.getLangsMember(m);
+    var langs = await bot.getLangsMember(m.id).catch(console.error);
 
     var embed = new bot.libs.discord.MessageEmbed()
         .setColor(bot.infoColor)
         .setTitle(":dagger: | New Empires - lang")
         .setFooter({ text: bot.footerAuthor.text + " | " + lang.toUpperCase(), iconURL: bot.footerAuthor.iconURL })
-        .addField("Lang" + (lang == "fr" ? "ue" : ""), (lang == "fr" ? ("La.les langue.s de <@" + m.id + "> est.sont ") : ("<@" + m.id + ">'s language.s is.are ")) + "**" + lStr + "**")
+        .addField("Lang" + (lang == "fr" ? "ue" : ""), (lang == "fr" ? ("La.les langue.s de <@" + m.id + "> est.sont ") : ("<@" + m.id + ">'s language.s is.are ")) + "**" + langs.join(" & ") + "**")
         .setThumbnail(m.user.avatarURL());
 
     interaction.reply({ embeds: [embed] });
